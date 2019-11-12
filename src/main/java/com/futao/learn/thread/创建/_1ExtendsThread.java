bf6e16b1c5aaa11c16f0362b008c4b09b58b3338
futao <1185172056@qq.com>
2019-11-12 18:55:28 +0800

    线程的创建

---
+++ src/main/java/com/futao/learn/thread/创建/_1ExtendsThread.java
@@ -0,0 +1,24 @@
+package com.futao.learn.thread.创建;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 创建多线程方式1-继承Thread
+ *
+ * @author futao
+ * Created on 2019/11/12.
+ */
+@Slf4j
+public class _1ExtendsThread extends Thread {     //1.继承自Thread类
+
+    @Override
+    public void run() {                         //2.重写run()方法
+        log.info("线程启动。。。");
+    }
+
+    public static void main(String[] args) {
+        _1ExtendsThread extendsThread = new _1ExtendsThread();      //该对象即是线程对象
+        extendsThread.setName("继承Thread方式");                //设置线程名称
+        extendsThread.start();                                  //3.调用start()方法启动线程(注意这里不是调用run()方法，如果调用了run()方法相当于调用普通方法，并不会启动新的线程)
+    }
+}
