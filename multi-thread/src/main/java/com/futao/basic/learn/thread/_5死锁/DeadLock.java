e03559b2e99d19c3800413655573fd0021e1f5ef
dark <1185172056@qq.com>
2019-11-21 22:41:55 +0800

    增加死锁概率

--- multi-thread/src/main/java/com/futao/basic/learn/thread/_5死锁/DeadLock.java
+++ multi-thread/src/main/java/com/futao/basic/learn/thread/_5死锁/DeadLock.java
@@ -2,6 +2,8 @@ package com.futao.basic.learn.thread._5死锁;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 死锁示例
  * 死锁：不同的线程分别占用对方需要的资源不放弃，且都在等待对方放弃自己需要的资源
@@ -12,14 +14,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DeadLock {
 
-    static Class<DeadLock> lock1 = DeadLock.class;
-    static Class<Object> lock2 = Object.class;
+    private static final Class<DeadLock> LOCK_1 = DeadLock.class;
+    private static final Class<Object> LOCK_2 = Object.class;
 
     public static void main(String[] args) throws InterruptedException {
         new Thread(() -> {
-            synchronized (lock1) {          //获取锁1
+            synchronized (LOCK_1) {          //获取锁1
                 log.info("获取到锁1");
-                synchronized (lock2) {      //获取锁2，但是此时锁2已经被第二个线程持有，所以会一直处于等待状态
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK_2) {      //获取锁2，但是此时锁2已经被第二个线程持有，所以会一直处于等待状态
                     log.info("获取到锁2");
                 }
             }
@@ -32,9 +39,14 @@ public class DeadLock {
         //TimeUnit.MILLISECONDS.sleep(100L);
 
         new Thread(() -> {
-            synchronized (lock2) {      //获取到锁2
+            synchronized (LOCK_2) {      //获取到锁2
                 log.info("获取到锁2");
-                synchronized (lock1) {      //获取锁1，但是因为线程1还处在运行状态，所以会一直等待线程1释放锁
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK_1) {      //获取锁1，但是因为线程1还处在运行状态，所以会一直等待线程1释放锁
                     log.info("获取到锁1");
                 }
             }
