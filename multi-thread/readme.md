a7a3463866db3ece0d4c281daba96c6b05089793
futao <1185172056@qq.com>
2019-11-27 09:58:14 +0800

    update pom desc

--- multi-thread/readme.md
+++ multi-thread/readme.md
@@ -1,1 +1,2 @@
 * 为什么 `wait()`, `notify()`, `notifyAll()`定义在Object而不是Thread
+* 什么是多线程的上下文切换
--- multi-thread/src/test/java/com/futao/basic/learn/thread/Normal.java
+++ multi-thread/src/test/java/com/futao/basic/learn/thread/Normal.java
@@ -3,12 +3,48 @@ package com.futao.basic.learn.thread;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author futao
  * Created on 2019/11/20.
  */
 @Slf4j
 public class Normal {
+
+    @Test
+    public void test3() throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+
+            for (int i = 0; i < 10000; i++) {
+                System.out.println(Thread.currentThread().getName() + "--" + i);
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+
+            for (int i = 0; i < 10000; i++) {
+                System.out.println(Thread.currentThread().getName() + "--" + i);
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(Thread.currentThread().getName() + "--" + i);
+        }
+        thread1.join();
+        thread2.join();
+    }
 
     @Test
     public void test1() {
